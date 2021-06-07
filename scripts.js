8bbc0874c420d48414c7fd39653e4b6e5550a1a3
hellolol <denniswang812@gmail.com>
2021-06-06 21:02:50 -0700

    finished everything except for reset button

--- scripts.js
+++ scripts.js
@@ -13,64 +13,78 @@ function computerPlay(){
 let pp = 0;
 let cp = 0;
 
-function round(playerSelection, computerSelection){
+function round(playerSelection){
     let ps = playerSelection.trim().toLowerCase();
-    let cs = computerSelection.trim().toLowerCase();
+    let cs = computerPlay().trim().toLowerCase();
     let pwin = false;
+    let cwin = false;
     if(ps === cs){
         return(`It's a tie! ${ps + " ties with " + cs}`)
     } else if(ps === "rock"){
         if(cs === "scissors"){
             pwin=true;
             pp++;
-            console.log("Player wins, rock beats scissors")
-        }else {
+        }else if (cs === "paper") {
+            cwin=true;
             cp++;
         }
     } else if(ps==="paper"){
         if(cs==="rock"){
             pwin=true;
             pp++;
-        } else {
+        } else if (cs === "scissors") {
+            cwin=true;
             cp++;
         }
     } else if(ps==="scissors"){
         if(cs==="paper"){
             pwin=true;
             pp++
-        } else {
+        } else if (cs === "rock"){
+            cwin=true;
             cp++;
         }
-    } else {
-        return "ps was WRONG";
     }
+
     if(pwin){
         return(`Player wins! ${ps + " beats " + cs}`);
-    } else {
+    } 
+    if(cwin) {
         return(`Computer wins! ${cs + " beats " + ps}`);
-    }
+    } 
+
 }
+const computerScore = document.querySelector('.cp');
+const playerScore = document.querySelector('.pp');
+const msg = document.querySelector('.msg');
+const buttons = document.querySelectorAll('button');
+const winMessage = document.querySelector('.winner');
+const restart = document.querySelector('.restart');
+buttons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        if(pp < 5 && cp < 5){
+            msg.textContent = round(button.className);
+        }
+        
+        playerScore.textContent = `Player: ${pp}`;
+        computerScore.textContent = `Computer: ${cp}`;
+        if(pp === 5 || cp === 5){
+            if(cp> pp){
+                winMessage.textContent = "Computer wins with 5 points!"
+            }else{
+                winMessage.textContent="Player wins with 5 points!"
+            }
+            restart.style.display = "block";
+            restart.addEventListener('click',console.log("reset"))
+        }
+    });
+});
 
-function game(){
-    const buttons = document.querySelectorAll('button');
-
-    let pw = 0;
-    let cw = 0;
-    for(var i = 0; i < 5; i++){
-        let playerSelection = "";
-        const computerSelection = computerPlay();
-
-        buttons.forEach((button)=>{
-            button.addEventListener('click', () => {
-                playerSelection = button.className;
-                round(playerSelection,computerSelection);
-            });
-        });
-    }
-    if(pp==5){
-        console.log(`Player wins! They had ${pw +" wins while Computer had " + cw}`);
-    } else if (cp==5){
-        console.log(`Computer wins! They had ${cw +" wins while Player had " + pw}`);
-    }
+function reset(){
+    cp = 0;
+    pp = 0;
+    playerScore.textContent = `Player:`;
+    computerScore.textContent = `Computer:`;
+    restart.style.display = "none";
+    winMessage.textContent = ""
 }
-game();
